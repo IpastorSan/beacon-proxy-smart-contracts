3664b8344388c75ab1037552534fa0a0bfd4e3e4
IpastorSan <ignaciopastorsan@gmail.com>
2023-12-19 13:17:45 +0100

    first setup of the base contracts

---
+++ src/BeaconProxyFactory.sol
@@ -0,0 +1,44 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BeaconProxyFactory is Ownable {
+    uint256 private _proxyId;
+    address private _beacon;
+
+    constructor (address beacon) Ownable(msg.sender) {
+        _beacon = beacon;
+    }
+
+    function createNewProxy(
+        string memory name, 
+        string memory symbol, 
+        string memory baseURI, 
+        uint256 maxSupply, 
+        uint256 price
+    ) external returns (address) {
+        BeaconProxy proxy = new BeaconProxy{salt: proxyId}(_beacon, "");
+        proxy.initialize(name, symbol, baseURI, maxSupply, price, msg.sender);
+        _proxyId++;
+        return address(proxy);
+    }
+    function getProxyAddress(uint256 proxyId) external view returns (address) {
+        bytes memory bytecode = type(BeaconProxy).creationCode;
+        bytecode = abi.encodePacked(bytecode, abi.encode(_beacon,""));
+        bytes32 hash = keccak256(
+            abi.encodePacked(
+                bytes1(0xff),
+                address(this),
+                proxyId,
+                keccak256(bytecode)
+            )
+        );
+
+        return address(uint160(uint256(hash)));
+    }
+    function getCurrentProxyId() external view returns (uint256) {
+        return _proxyId;
+    }
+}
---
+++ src/NFTV1Implementation.sol
@@ -0,0 +1,63 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "@openzeppelin-contracts-upgradeable/contracts/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
+
+
+error MintPriceNotPaid();
+error SupplyExceeded();
+
+contract NFTV1 is ERC721Upgradeable, OwnableUpgradeable {
+    uint256 public immutable max_supply;
+    uint256 public immutable price;
+    uint256 public tokenId;
+    string public baseTokenURI;
+
+    constructor() ERC721("RandomNFT721", "RNFT") {
+        _disableInitializable();
+    }
+
+    function initializer(
+        string memory name,
+        string memory symbol,
+        string memory baseURI,
+        uint256 initial_max_supply, 
+        uint256 initial_price,
+        address owner
+    ) external initializer {
+
+        __ERC721_init_unchained(name, symbol);
+        __Ownable_init_unchained(owner);
+
+        baseTokenURI = baseURI;
+        max_supply = initial_max_supply;
+        price = initial_price;
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        uint256 currentId = tokenId;
+
+        if(currentId + amount > max_supply) revert SupplyExceeded();
+        if(msg.value != price * amount) revert MintPriceNotPaid();
+
+        tokenId += amount;
+
+        for (uint256 i= 0; i < amount; i++){
+            _mint(to, currentId + i);
+        }
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+       return baseTokenURI;
+    }
+
+    function withdraw() external onlyOwner {
+     uint256 balance = address(this).balance;
+     require(balance > 0, "No ether left to withdraw");
+
+     (bool success, ) = payable(owner()).call{value: balance}("");
+
+     require(success, "Transfer failed.");
+    }
+}
